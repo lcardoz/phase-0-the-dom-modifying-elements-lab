55a2a7918cf11b98bb2ed33c7d6dd070be405aee
Leah Cardoz <leahcardoz@gmail.com>
2022-10-12 12:04:52 -0400

    commit tree bed086db4181baeb14a5996f05f85cc4d2da0911

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+newHeader.id = 'victory';
+
+newHeader.textContent = "Leah is the champion";
